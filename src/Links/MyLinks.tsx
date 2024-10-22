dda812d6c4ed203a7adc55a271993405d2f7d40f
laynexx <argo150808@gmail.com>
2024-10-22 15:36:26 +0300

    added links and fixed some design problems

--- src/Links/MyLinks.tsx
+++ src/Links/MyLinks.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import './MyLinks.css'
+import telega from '../images/mylinks/Frame 2.png'
+import github from '../images/mylinks/ghlogo.png'
+import dslogo from '../images/mylinks/dslogo.png'
+import {Image} from "antd";
 
 const MyLinks: React.FC = () =>{
     return(
@@ -8,9 +12,19 @@ const MyLinks: React.FC = () =>{
                 <h1>
                     MyLinks:
                 </h1>
+
             </div>
             <div className="images-links">
-                <img/>
+                <a href={"https://t.me/exldlxx_bezdarnost"}>
+                    <img className={"logo"} src={telega} alt="Telegram Logo"/>
+                </a>
+                <a href={"https://github.com/Laynexx-ns"}>
+                    <img className={"logo"} src={github} alt="GitHub Logo"/>
+                </a>
+                <a href={"https://github.com/Laynexx-ns"}>
+                    <img className={"logo"} src={dslogo} alt={"Discord Logo"}/>
+                </a>
+
             </div>
         </div>
     )
