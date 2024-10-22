6c928f4ec5a4f9113947bec212ec682ee9ebaaeb
laynexx <argo150808@gmail.com>
2024-10-22 16:45:07 +0300

    fixed page size

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import ActionButton from "antd/es/_util/ActionButton";
-import {Button, Space} from "antd";
 import './AppStyle.css'
-
 import MainText from "./MainText/MainText";
-import MainBack from "./MainBack/MainBack";
 import MyLinks from "./Links/MyLinks";
 import MyStack from "./Links/MyStack";
-import FullBack from "./MainBack/fullBack";
 import myLogo from './images/logo.png'
 import FavoriteMusic from "./Links/FavoriteMusic";
 
